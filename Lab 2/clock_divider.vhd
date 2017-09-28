483864ca844a808419def4eb6d60f41bab53a27b
ardenoh <aoh.301@gmail.com>
2017-09-28 16:19:42 -0600

    done clock_divider file

--- Lab 2/clock_divider.vhd
+++ Lab 2/clock_divider.vhd
@@ -6,6 +6,10 @@ entity clock_divider is
            reset : in  STD_LOGIC;
            enable: in STD_LOGIC;
 			     kHz: out STD_LOGIC
+			singleSecPort: out STD_LOGIC_VECTOR(3 downto 0);
+			tenSecPort: out STD_LOGIC_VECTOR(2 downto 0);
+			minutePort: out STD_LOGIC_VECTOR(3 downto 0);
+			tenMinutePort: out STD_LOGIC_VECTOR(2 downto 0);	
 		   -- Add output buses (STD_LOGIC_VECTOR(x downto y))
 		   -- here to hold values of single seconds, tens of seconds,
 		   -- minutes, and tens of minutes. Consult Lab 1 intructions
@@ -19,6 +23,18 @@ signal 	i_enable: STD_LOGIC;
 signal 	kilohertz: STD_LOGIC;
 signal 	hundredhertz: STD_LOGIC;
 signal	tenhertz: STD_LOGIC;
+signal  onehertz: STD_LOGIC;
+signal  onetenthhertz: STD_LOGIC;
+
+signal onehertz: STD_LOGIC;
+signal tensec: STD_LOGIC;
+signal onemin: STD_LOGIC;
+signal tenmin: STD_LOGIC;
+
+signal singleSecValue: STD_LOGIC_VECTOR(3 downto 0);
+signal tenSecValue: STD_LOGIC_VECTOR(2 downto 0);
+signal minuteValue: STD_LOGIC_VECTOR(3 downto 0);
+signal tenMinuteValue: STD_LOGIC_VECTOR (2 downto 0);
 -- Add signals here
 
 -- Components:
@@ -72,9 +88,76 @@ port map (
 				zero => tenhertz,
 				value => open			-- Leave open since we won't display this value
 );
+oneHzClk: downcounter
+generic map(
+				period => (10-1),
+				WIDTH => 4
+)
+port map(
+				clk => clk,
+				reset => reset,
+				enable => tenhertz,
+				zero => onehertz,
+				value => open
+);
+singleSecClk: downcounter
+generic map(
+				period => (10-1),
+				WIDTH => 4
+)
+port map(
+				clk => clk,
+				reset => reset,
+				enable => onehertz,
+				zero => tensec,
+				value => singleSecValue
+);
+tenSecondsClock: downcounter
+generic map(
+				period => (6-1),
+				WIDTH => 3
+)
+port map(
+				clk => clk,
+				reset => reset,
+				enable => tensec,
+				zero => onemin,
+				value => tenSecValue
+);
+singleMinuteClock: downcounter
+generic map(
+				period => (10-1),
+				WIDTH => 4
+)
+port map(
+				clk => clk,
+				reset => reset,
+				enable => onemin,
+				zero => tenmin;
+				value => minuteValue
+);
+tensMinuteClock: downcounter
+generic map(
+				period => (6-1),
+				WIDTH => 3
+)
+port map(
+				clk => clk,
+				reset => reset,
+				enable => tenmin,
+				zero => open,
+				value => tenMinuteValue;
+);
+
 
 -- Connect internal signals to outputs
+
 kHz <= kilohertz;
 i_enable <= kilohertz and enable;
+
+singleSecPort <= singleSecValue;
+tenSecPort <= tenSecValue;
+minutePort <= minuteValue;
+tenMinutePort <= tenMinuteValue;
 
 end Behavioral;
