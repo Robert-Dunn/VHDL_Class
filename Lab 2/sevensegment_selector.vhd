8add89a0243f1f895489ab4d909598894d9fa734
ardenoh <aoh.301@gmail.com>
2017-09-28 16:18:40 -0600

    done both selector files

--- Lab 2/sevensegment_selector.vhd
+++ Lab 2/sevensegment_selector.vhd
@@ -10,20 +10,30 @@ entity sevensegment_selector is
 end sevensegment_selector;
 
 architecture Behavioral of sevensegment_selector is
-	signal d, q: STD_LOGIC_VECTOR(3 downto 0);
+    signal d, q: STD_LOGIC_VECTOR(3 downto 0);
 begin
 
 dffs: process(reset, clk)
 begin
-	if (reset = '1') then
-		-- Fill in the reset state values for q
-	elsif (rising_edge(clk)) then
-		if (switch = '1') then
-			-- Propagate signals through the DFF
-			-- From input to output
-		else 
-		end if;
-	end if;
+    if (reset = '1') then
+       q <="1000";
+        -- Fill in the reset state values for q
+    elsif (rising_edge(clk)) then
+        if (switch = '1') then
+           d(0)<=q(3);
+           q(0)<=d(0);
+           d(1)<=q(0);
+           q(1)<=d(1);
+           d(2)<=q(1);
+           q(2)<=d(2);
+           d(3)<=q(2);
+           q(3)<=d(3);
+
+            -- Propagate signals through the DFF
+            -- From input to output
+        else 
+        end if;
+    end if;
 end process;
 
 -- Connect the DFFs into a chain/loop
--- Lab 2/tb_selector.vhd
+++ Lab 2/tb_selector.vhd
@@ -1,13 +1,13 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+
 ENTITY tb_selector IS
 END tb_selector;
  
 ARCHITECTURE behavior OF tb_selector IS 
- 
+
     -- Component Declaration for the Unit Under Test (UUT)
- 
+
     COMPONENT sevensegment_selector
     PORT(
          clk : IN  std_logic;
@@ -23,15 +23,15 @@ ARCHITECTURE behavior OF tb_selector IS
    signal switch : std_logic := '0';
    signal reset : std_logic := '0';
 
- 	--Outputs
+    --Outputs
    signal output : std_logic_vector(3 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
- 
+
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
+    -- Instantiate the Unit Under Test (UUT)
    uut: sevensegment_selector PORT MAP (
           clk => clk,
           switch => switch,
@@ -42,31 +42,36 @@ BEGIN
    -- Clock process definitions
    clk_process :process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
    end process;
-	
-	switch_process: process
-	begin
-		-- Write the contents of this process
-		-- switch needs to pulse, but slower than clk
-	end process;
- 
+
+    switch_process: process
+    begin
+        switch <= '1';
+        wait for clk_period;
+        switch <= '0';
+        wait for 1 ms;
+        -- Write the contents of this process
+        -- switch needs to pulse, but slower than clk
+    end process;
+
 
    -- Stimulus process
    stim_proc: process
-   begin		
+   begin        
       -- hold reset state for 100 ns.
-		reset <= '0';
-      wait for 100 ns;	
-		reset <= '1';
+        reset <= '0';
+      wait for 100 ns;  
+        reset <= '1';
       wait for clk_period*10;
-		reset <= '0';
-      -- insert stimulus here 
+        reset <= '0';
+      -- insert stimulus here
+	
 
       wait for clk_period*100;
    end process;
 
-END; 
+END;
