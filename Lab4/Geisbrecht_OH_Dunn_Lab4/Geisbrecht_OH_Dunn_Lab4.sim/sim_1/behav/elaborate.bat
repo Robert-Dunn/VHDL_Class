30a4bba91175fa1f8972131f3b0fb5a7734035b0
Robert Dunn <rdskifreak@gmail.com>
2017-11-05 22:22:02 -0700

    removed all vivado files from repo.

--- Lab4/Geisbrecht_OH_Dunn_Lab4/Geisbrecht_OH_Dunn_Lab4.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto 317363e0b2b04bd589e31380fc1f5318 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot clock_divider_behav xil_defaultlib.clock_divider -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
